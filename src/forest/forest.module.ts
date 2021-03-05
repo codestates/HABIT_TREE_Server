2547b2179c7793caa331f56f827fd8560b304f70
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-03-05 16:05:03 +0900

    [SERVER] [FIX] https

--- src/forest/forest.module.ts
+++ src/forest/forest.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Forest } from 'src/entity/forest.entity';
 import { ForestController } from './forest.controller';
 import { ForestService } from './forest.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Forest])],
+  imports: [TypeOrmModule.forFeature([Forest]), AuthModule],
   controllers: [ForestController],
   providers: [ForestService],
   exports: [ForestService],
