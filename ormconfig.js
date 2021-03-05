4518ce655ad3e952efb9b5468c06e90bc653f463
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-03-05 13:39:13 +0900

    [SERVER] [FIX] https

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
     type: 'mysql',
     host: 'habbittree.cudb6vnblhbf.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
-    username: 'habittree',
+    username: 'admin',
     password: process.env.DB_PASWORD,
     database: 'typeorm2',
     entities: ['dist/**/*.entity{.ts,.js}'],
