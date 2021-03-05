bb49d2b551d68db3e893d5210e7d04dab0bce1ae
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-03-05 11:04:49 +0900

    [SERVER] [FIX] https

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../../key.pem'),
+    cert: fs.readFileSync('../../cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors({
     origin: 'http://localhost:3001',
   });
@@ -25,6 +32,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
